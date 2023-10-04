014eba75412be83901881230bc791d5b316f65e1
rlopgar <rlopgar599@g.educaand.es>
2023-10-04 10:09:57 +0200

    Version final

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner (System.in);
+
+        System.out.println("Introduce los grados :");
+
+        double Grados = entrada.nextDouble();
+
+        double f = (9/5)*Grados+32;
+        double k = Grados+273.15;
+
+        System.out.println("La temperatura en grados Fahrenheit es: "+ f + " y la temperatura en grados Kelvin es : "+k);
+
+
+
+
+
+    }
+
 }
